a739b151b8f70b5050f2325c8398ef9845818ddb
Leticia Marques <leticiaccmarques@hotmail.com>
2022-07-16 21:33:40 -0300

    Adicionada branch de development

--- src/service-course-template/node/clients/index.ts
+++ src/service-course-template/node/clients/index.ts
@@ -1,10 +1,7 @@
 import { IOClients } from '@vtex/api'
-import Analytics from './analyticsClient'
 
 
 // Extend the default IOClients implementation with our own custom clients.
 export class Clients extends IOClients {
-  public get analytics() {
-    return this.getOrSet('analytics', Analytics)
-  }
+
 }
--- src/service-course-template/node/handlers/analytics.ts
+++ src/service-course-template/node/handlers/analytics.ts
@@ -1,9 +1,7 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
-  const {
-    clients: { analytics },
-  } = ctx
   ctx.status = 200
-  ctx.body = await analytics.getLiveUsers()
+  ctx.body = 'OK'
   ctx.set('cache-control', 'no-cache')
+
   await next()
 }
